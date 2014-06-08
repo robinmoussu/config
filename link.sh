24172942ed6137ee7c1a717a5701b0e4198f3ba3
Robin Moussu <robin.moussu+github@gmail.com>
2014-06-08 23:12:36 +0200

    clean up

--- link.sh
+++ link.sh
@@ -8,7 +8,8 @@ COMMON_DIR_="/home/"$COMMON_
 
 LINK_=`ls -A $COMMON_DIR_ | grep -v .sh`
 
-USER_="/home/"`ls /home | grep -v $COMMON_`" /root"
+#USER_="/home/"`ls /home | grep -v $COMMON_`" /root"
+USER_="/home/robin_arch /root"
 
 for CURRENT_USER_ in $USER_; do
 	for CURRENT_LINK_ in $LINK_; do 

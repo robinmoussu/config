90566d454bb74951ffcbe9e4b9f2faddf2507cbe
Robin Moussu <robin.moussu+github@gmail.com>
2014-06-09 11:28:02 +0200

    bugfix

--- link.sh
+++ link.sh
@@ -6,13 +6,15 @@
 COMMON_="config"
 COMMON_DIR_="/home/"$COMMON_
 
-LINK_=`ls -A $COMMON_DIR_ | grep -v .sh`
+LINK_=`ls -A $COMMON_DIR_ | egrep -v -e .sh$ -e README`
 
 #USER_="/home/"`ls /home | grep -v $COMMON_`" /root"
 USER_="/home/robin_arch /root"
 
 for CURRENT_USER_ in $USER_; do
-	for CURRENT_LINK_ in $LINK_; do 
+    echo ---- $CURRENT_USER_ ----
+	for CURRENT_LINK_ in $LINK_; do
+		echo ln -sb $COMMON_DIR_/$CURRENT_LINK_ $CURRENT_USER_/$CURRENT_LINK_
 		ln -sb $COMMON_DIR_/$CURRENT_LINK_ $CURRENT_USER_/$CURRENT_LINK_
 	done
 done

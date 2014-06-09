b8628e7bcb15d6b6b1c21f94c793d42f43641b79
Robin Moussu <robin.moussu+github@gmail.com>
2014-06-09 19:26:35 +0200

    bugfix

--- vim.sh
+++ vim.sh
@@ -4,6 +4,6 @@ COMMON="/home/config"
 rm -rf $COMMON/.vim/bundle/Vundle.vim
 git clone https://github.com/gmarik/Vundle.vim.git $COMMON/.vim/bundle/Vundle.vim
 mv -b $COMMON/.vimrc $COMMON/.vimrc.backup
-mv -f $COMMON/.vimrc.install $COMMON/.vimrc
+cp -f $COMMON/.vimrc.install $COMMON/.vimrc
 vim +BundleInstall +BundleClean +BundleUpdate +wqa
 mv $COMMON/.vimrc.backup $COMMON/.vimrc

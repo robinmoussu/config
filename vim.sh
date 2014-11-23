274b89b7125f022bb5c1d1668214f16a67b60d12
robin moussu <robin.moussu+git@gmail.com>
2014-11-23 23:03:47 +0100

    fix

--- vim.sh
+++ vim.sh
@@ -1,3 +1,3 @@
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim  -u .vimrc.install +PluginInstall +'source ~/.vimrc'  +BundleInstall +BundleClean +BundleUpdate +qa
+vim  -u .vimrc.install +PluginInstall +'source ~/.vimrc'  +BundleInstall +BundleClean +qa

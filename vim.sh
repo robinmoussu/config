2ff994214581431d9005bdd8557bd4bfd71ff4fc
robin moussu <robin.moussu+git@gmail.com>
2014-11-23 23:01:30 +0100

    better vim install script

--- vim.sh
+++ vim.sh
@@ -1,9 +1,3 @@
 
-COMMON="/nfs/home/rmoussu"
-
-rm -rf $COMMON/.vim/bundle/Vundle.vim
-git clone https://github.com/gmarik/Vundle.vim.git $COMMON/.vim/bundle/Vundle.vim
-mv -b $COMMON/.vimrc $COMMON/.vimrc.backup
-cp -f $COMMON/.vimrc.install $COMMON/.vimrc
-vim +BundleInstall +BundleClean +BundleUpdate +wqa
-mv $COMMON/.vimrc.backup $COMMON/.vimrc
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim  -u .vimrc.install +PluginInstall +'source ~/.vimrc'  +BundleInstall +BundleClean +BundleUpdate +qa
